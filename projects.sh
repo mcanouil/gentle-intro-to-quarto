69529ecb250004d9699b872cce1af27229e32ae3
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2023-10-18 21:23:20 +0200

    feat: webr example and links to vscode (#2)

--- projects.sh
+++ projects.sh
@@ -8,3 +8,20 @@ quarto create project website --no-open --no-prompt Website
 quarto create project blog --no-open --no-prompt Blog
 quarto create project book --no-open --no-prompt Book
 quarto create project manuscript --no-open --no-prompt Manuscript
+
+quarto create project default --no-open --no-prompt webr
+echo '---
+title: webR in Quarto HTML Documents
+format: html
+engine: knitr
+filters:
+  - webr
+---
+
+This is a webr-enabled code cell in a Quarto HTML document.
+
+```{webr-r}
+fit = lm(mpg ~ am, data = mtcars)
+summary(fit)
+```
+' > webr/Webr.qmd
